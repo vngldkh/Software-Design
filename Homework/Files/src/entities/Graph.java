65a3a49ea09153b1748ed5a681f77735ef2aa714
vngldkh <idgladkikh@edu.hse.ru>
2023-01-07 14:23:57 +0300

    Added Graph class

---
+++ Homework/Files/src/entities/Graph.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Graph {
+    private final HashMap<String, Vertex> vertexPool;
+    private boolean cycled;
+
+    public Graph() {
+        vertexPool = new HashMap<>();
+        cycled = false;
+    }
+
+    public void Add(String from, String to) {
+        if (cycled) {
+            return;
+        }
+        if (!vertexPool.containsKey(from)) {
+            vertexPool.put(from, new Vertex(from));
+        }
+        if (!vertexPool.containsKey(to)) {
+            vertexPool.put(to, new Vertex(to));
+        }
+        var departure = vertexPool.get(from);
+        var destination = vertexPool.get(to);
+        cycled |= departure.AddChildren(destination);
+        if (cycled) {
+            return;
+        }
+        destination.AddParent(departure);
+    }
+
+    public boolean Cycled() {
+        return cycled;
+    }
+
+    public ArrayList<String> Order() {
+        ArrayList<String> answer = new ArrayList<>();
+        return answer;
+    }
+}

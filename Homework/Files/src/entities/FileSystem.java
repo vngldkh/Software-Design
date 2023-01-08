2f8bf2df026b50efcf89f7e1c5247243a3f17437
vngldkh <idgladkikh@edu.hse.ru>
2023-01-08 21:16:56 +0300

    Added comments

--- Homework/Files/src/entities/FileSystem.java
+++ Homework/Files/src/entities/FileSystem.java
@@ -3,18 +3,44 @@ package entities;
 import system.ExceptionMessages;
 import system.Reader;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+/***
+ * Class of the file system.
+ */
 public class FileSystem {
+    /***
+     * Object of the Reader class.
+     * Needed to read information about the file system.
+     */
     private final Reader reader;
+    /***
+     * Graph equal to the file system.
+     */
     private final Graph graph;
+    /***
+     * Ordered list of the file system.
+     */
     private LinkedList<String> order;
+    /***
+     * Pool of the files.
+     */
     private final HashMap<String, FileInfo> filesDictionary;
+    /***
+     * Informs whether the system is correct.
+     */
     private boolean correct;
 
+    /***
+     * Constructor.
+     * 1. Reads the information about the file system. Fill the pool.
+     * 2. Builds up equivalent graph.
+     * 3. Makes an ordered list (based on the graph).
+     * If there's a problem occurred during the construction, user will be informed.
+     * @param initDirectory Path to the root folder.
+     */
     public FileSystem(String initDirectory) {
         reader = new Reader(initDirectory);
         graph = new Graph();
@@ -36,6 +62,9 @@ public class FileSystem {
         order = graph.Order();
     }
 
+    /***
+     * Reads the information about the file system (all its files) and fills the pool up.
+     */
     private void ListAllFiles() {
         var files = reader.ListAllFiles();
         correct = reader.IsCorrect();
@@ -47,6 +76,9 @@ public class FileSystem {
         }
     }
 
+    /***
+     * Builds up a graph equivalent to file system.
+     */
     private void SetUpGraph() {
         for (Map.Entry<String, FileInfo> pair : filesDictionary.entrySet()) {
             graph.Add(pair.getKey());
@@ -63,6 +95,10 @@ public class FileSystem {
         }
     }
 
+    /***
+     * Concatenate files' texts according the ordered list.
+     * @return Concatenated text.
+     */
     public String MakeText() {
         StringBuilder text = new StringBuilder();
         for (String fileName : order) {
@@ -71,10 +107,18 @@ public class FileSystem {
         return text.toString();
     }
 
+    /***
+     * Getter to the ordered list.
+     * @return Ordered list.
+     */
     public LinkedList<String> GetOrder() {
         return order;
     }
 
+    /***
+     * Informs the 'external world' whether the file system is correct.
+     * @return 'true' if it's correct and 'false' otherwise.
+     */
     public boolean IsCorrect() {
         return correct;
     }

e334b80e6825fc1040ad0f1f3927744d112b7a23
vngldkh <idgladkikh@edu.hse.ru>
2023-01-08 20:45:01 +0300

    Added comments

--- Homework/Files/src/system/Console.java
+++ Homework/Files/src/system/Console.java
@@ -4,15 +4,30 @@ import entities.FileSystem;
 
 import java.util.Scanner;
 
+/***
+ * Class to interact with user through the console.
+ */
 public class Console {
+    /***
+     * Object of Scanner class to read text from the console.
+     */
     private static final Scanner scanner = new Scanner(System.in);
 
+    /***
+     * Static method to initialize work of the program.
+     * Reads the path to the root folder.
+     * @return Path to the root folder.
+     */
     public static String Init() {
         System.out.println("Enter the path to the initial directory: ");
         System.out.print(">> ");
         return scanner.nextLine().replace('/', '\\');
     }
 
+    /***
+     * Static method to find out whether user wants to continue.
+     * @return 'true' if user wants to continue and 'false' otherwise.
+     */
     public static boolean Continue() {
         System.out.print("If you want to continue write 'y'.\nOr anything else otherwise.\n>> ");
         String answer = scanner.nextLine();
@@ -20,6 +35,11 @@ public class Console {
         return "y".equals(answer);
     }
 
+    /***
+     * Static method to display information about file system:
+     * ordered list and concatenated text.
+     * @param fileSystem Object of FileSystem class.
+     */
     public static void DisplayFileSystem(FileSystem fileSystem) {
         if (!fileSystem.IsCorrect()) {
             ExceptionMessages.Message(3);
--- Homework/Files/src/system/ExceptionMessages.java
+++ Homework/Files/src/system/ExceptionMessages.java
@@ -1,12 +1,17 @@
 package system;
 
 public class ExceptionMessages {
+    // Exception messages text.
     private static final String NO_DIRECTORY = "There's no such directory!\n";
     private static final String NO_ACCESS = "You don't have an access to files or directories there!\n";
     private static final String CYCLED = "The graph is cycled!\n";
     private static final String UNEXPECTED = "Unexpected exception occurred!\n";
     private static final String INCORRECT = "The file system is incorrect!\nImpossible to display!\n";
 
+    /***
+     * Inform user about the occurred exception.
+     * @param code Code of the occured exception.
+     */
     public static void Message(int code) {
         String message;
         switch (code) {

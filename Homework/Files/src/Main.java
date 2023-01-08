69cb3165d4146024b271e17a028ade076029a870
vngldkh <idgladkikh@edu.hse.ru>
2023-01-08 19:12:49 +0300

    Main class  has acquired a working appearance

--- Homework/Files/src/Main.java
+++ Homework/Files/src/Main.java
@@ -1,14 +1,12 @@
-import entities.Graph;
+import entities.FileSystem;
+import system.Console;
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.Add("A", "B");
-        graph.Add("A", "C");
-        graph.Add("B", "C");
-        graph.Add("C", "D");
-        graph.Add("D", "E");
-        graph.Add("E", "B");
-        System.out.println(graph.Cycled());
+        do {
+            String initialDirectory = Console.Init();
+            FileSystem fileSystem = new FileSystem(initialDirectory);
+            Console.DisplayFileSystem(fileSystem);
+        } while (Console.Continue());
     }
 }

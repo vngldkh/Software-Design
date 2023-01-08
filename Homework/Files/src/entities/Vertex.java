c5fb3195ab91ebe4d41717a4e00e4ad408da1d06
vngldkh <idgladkikh@edu.hse.ru>
2023-01-08 21:07:28 +0300

    Added comments

--- Homework/Files/src/entities/Vertex.java
+++ Homework/Files/src/entities/Vertex.java
@@ -2,21 +2,46 @@ package entities;
 
 import java.util.HashSet;
 
+/***
+ * Class of graph vertex.
+ */
 final class Vertex {
+    /***
+     * Vertex name.
+     */
     final String name;
+    /***
+     * Set of vertex's ancestors.
+     */
     private final HashSet<Vertex> ancestors;
+    /***
+     * Set of vertex's children.
+     */
     private final HashSet<Vertex> children;
 
+    /***
+     * Constructor.
+     * @param name Name of the vertex.
+     */
     Vertex(String name) {
         this.name = name;
         children = new HashSet<>();
         ancestors = new HashSet<>();
     }
 
+    /***
+     * Adds parent to the vertex (and all its children).
+     * @param parent Parent vertex.
+     */
     void AddParent(Vertex parent) {
         updateAncestors(this, parent);
     }
 
+    /***
+     * Adds parent and all missing ancestors to the vertex (and all its children).
+     * @param current Vertex to update.
+     * @param ancestor Its new parent.
+     */
     private static void updateAncestors(Vertex current, Vertex ancestor) {
         current.ancestors.addAll(ancestor.ancestors);
         current.ancestors.add(ancestor);
@@ -28,27 +53,45 @@ final class Vertex {
         }
     }
 
+    /***
+     * Adds child to the vertex.
+     * @param child Child vertex.
+     * @return 'true' if the child is one of the vertex's ancestors.
+     */
     boolean AddChildren(Vertex child) {
         children.add(child);
         return ancestors.contains(child);
     }
 
+    /***
+     * Getter of children set.
+     * @return Set of the children.
+     */
     HashSet<Vertex> GetChildren() {
         return children;
     }
 
+    /***
+     * Informs whether the vertex has ancestors.
+     * @return 'true' if there are some ancestors and 'false' otherwise.
+     */
     boolean HasAncestors() {
         return !ancestors.isEmpty();
     }
-    boolean HasChildren() {
-        return !children.isEmpty();
-    }
 
+    /***
+     * Override hashCode method to make this class work correctly with HashSet and HashMap.
+     * @return Hash code.
+     */
     @Override
     public int hashCode() {
         return name.hashCode();
     }
 
+    /***
+     * Override equals method to make this class work correctly with HashSet and HashMap.
+     * @return 'true' if the other object is the same vertex and 'false' otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (o instanceof Vertex vertex) {
